2bd27250bc20eca539e528cf13d9f8f0f072c85b
Mitchell Cunningham <mitch@compnamevar.lan>
2019-06-03 10:23:30 +1000

    scrollama populate text

--- scrollama-cytoscape/cy-script.js
+++ scrollama-cytoscape/cy-script.js
@@ -4,79 +4,79 @@ var cy = cytoscape({
   container: document.getElementById('cy'), // container to render in
 
   //elements: { // list of graph elements to start with
-    //nodes: [
-    //    { // node a
-    //   data: {
-    //     id: 'a'
-    //   }
-    // }, { // node b
-    //   data: {
-    //     id: 'b'
-    //   }
-    // }, {
-    //   data: {
-    //     id: 'd'
-    //   }
-    // }, {
-    //   data: {
-    //     id: 'e'
-    //   }
-    // }, {
-    //   data: {
-    //     id: 'f'
-    //   }
-    // }, {
-    //   data: {
-    //     id: 'g'
-    //   }
-    //   }
-    //],
-    // edges: [{ // edge ab
-    //   data: {
-    //     id: 'ab',
-    //     source: 'a',
-    //     target: 'b'
-    //   }
-    // }, {
-    //   data: {
-    //     id: 'bd',
-    //     source: 'b',
-    //     target: 'd'
-    //   }
-    // }, {
-    //   data: {
-    //     id: 'ad',
-    //     source: 'a',
-    //     target: 'd'
-    //   }
-    // }, {
-    //   data: {
-    //     id: 'df',
-    //     source: 'd',
-    //     target: 'f'
-    //   }
-    // }, {
-    //   data: {
-    //     id: 'ed',
-    //     source: 'e',
-    //     target: 'd'
-    //   }
-    // }, {
-    //   data: {
-    //     id: 'dg',
-    //     source: 'd',
-    //     target: 'g'
-    //   }
-    // }, {
-    //   data: {
-    //     id: 'fe',
-    //     source: 'f',
-    //     target: 'e'
-    //   }
+  //nodes: [
+  //    { // node a
+  //   data: {
+  //     id: 'a'
+  //   }
+  // }, { // node b
+  //   data: {
+  //     id: 'b'
+  //   }
+  // }, {
+  //   data: {
+  //     id: 'd'
+  //   }
+  // }, {
+  //   data: {
+  //     id: 'e'
+  //   }
+  // }, {
+  //   data: {
+  //     id: 'f'
+  //   }
+  // }, {
+  //   data: {
+  //     id: 'g'
+  //   }
+  //   }
+  //],
+  // edges: [{ // edge ab
+  //   data: {
+  //     id: 'ab',
+  //     source: 'a',
+  //     target: 'b'
+  //   }
+  // }, {
+  //   data: {
+  //     id: 'bd',
+  //     source: 'b',
+  //     target: 'd'
+  //   }
+  // }, {
+  //   data: {
+  //     id: 'ad',
+  //     source: 'a',
+  //     target: 'd'
+  //   }
+  // }, {
+  //   data: {
+  //     id: 'df',
+  //     source: 'd',
+  //     target: 'f'
+  //   }
+  // }, {
+  //   data: {
+  //     id: 'ed',
+  //     source: 'e',
+  //     target: 'd'
+  //   }
+  // }, {
+  //   data: {
+  //     id: 'dg',
+  //     source: 'd',
+  //     target: 'g'
+  //   }
+  // }, {
+  //   data: {
+  //     id: 'fe',
+  //     source: 'f',
+  //     target: 'e'
+  //   }
   //   }]
   //},
 
-  ready: function(e) {
+  ready: function (e) {
     console.info("cytoscape is ready");
   },
   minZoom: 1e-5,
@@ -124,9 +124,9 @@ var options = {
   pixelRatio: 1,
   name: 'cose',
   // Called on `layoutready`
-  ready: function() {},
+  ready: function () {},
   // Called on `layoutstop`
-  stop: function() {},
+  stop: function () {},
   // Whether to animate while running the layout
   animate: true,
   // The layout animates only after this many milliseconds
@@ -145,17 +145,17 @@ var options = {
   // Extra spacing between components in non-compound graphs
   componentSpacing: 100,
   // Node repulsion (non overlapping) multiplier
-  nodeRepulsion: function(node) {
+  nodeRepulsion: function (node) {
     return 400000;
   },
   // Node repulsion (overlapping) multiplier
   nodeOverlap: 10,
   // Ideal edge (non nested) length
-  idealEdgeLength: function(edge) {
+  idealEdgeLength: function (edge) {
     return 10;
   },
   // Divisor to compute edge forces
-  edgeElasticity: function(edge) {
+  edgeElasticity: function (edge) {
     return 100;
   },
   // Nesting factor (multiplier) to compute ideal edge length for nested edges
@@ -177,115 +177,113 @@ var options = {
 
 
 function createDegreeDist(arr) {
-  var a = [], b = [], prev;
+  var a = [],
+    b = [],
+    prev;
   arr.sort();
-  for ( var i = 0; i < arr.length; i++ ) {
-      if ( arr[i] !== prev ) {
-          a.push(arr[i]);
-          b.push(1);
-      } else {
-          b[b.length-1]++;
-      }
-      prev = arr[i];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] !== prev) {
+      a.push(arr[i]);
+      b.push(1);
+    } else {
+      b[b.length - 1]++;
+    }
+    prev = arr[i];
   }
 
   var maxDegree = Math.max.apply(null, a);
   var degreeFreq = [];
   var biter = 0;
 
-  var N = 10; 
-  Array.apply(null, {length: N}).map(Number.call, Number)
+  var N = 10;
+  Array.apply(null, {
+    length: N
+  }).map(Number.call, Number)
 
-  for ( var i = 0; i <= maxDegree; i++ ) {
-    if (a.includes(i)){
+  for (var i = 0; i <= maxDegree; i++) {
+    if (a.includes(i)) {
       degreeFreq.push(b[biter]);
       biter = biter + 1;
     } else {
       degreeFreq.push(0);
     }
   }
-  
-  var N = maxDegree+1; 
-  var intList = Array.apply(null, {length: N}).map(Number.call, Number);
-  var totalDegree = math.dot(degreeFreq,intList);
-  
+
+  var N = maxDegree + 1;
+  var intList = Array.apply(null, {
+    length: N
+  }).map(Number.call, Number);
+  var totalDegree = math.dot(degreeFreq, intList);
+
   return degreeFreq;
 }
 
-var createDegreeArray = function() {
+var createDegreeArray = function () {
   allElements = cy.elements();
-  var allEdges  = allElements.filter('edge');                
+  var allEdges = allElements.filter('edge');
   var allNodes = allElements.filter('node');
   window.degreeArray = [];
-  allNodes.filter(function(i,ele){
+  allNodes.filter(function (i, ele) {
     degreeArray.push(ele.degree());
   });
   window.nodeDegreeDist = createDegreeDist(degreeArray)
 }
 
-var addNode = function(nodeid) {
+var addNode = function (nodeid) {
   cy.add([{
     data: {
       id: nodeid
     }
-  }
-  ]);
+  }]);
 }
 
-var addLink = function(from_node_id,to_node_id) {
+var addLink = function (from_node_id, to_node_id) {
   cy.add([{
     data: {
       id: from_node_id + ' ' + to_node_id,
       source: from_node_id + '',
       target: to_node_id + ''
     }
-  }
-]);
+  }]);
 }
 
-var updateChartLayoutGraph = function() {
+var updateChartLayoutGraph = function () {
   createDegreeArray();
   cy.layout(options);
 }
 
-var addNextNodes = function(numNodes) {
+var addNextNodes = function (numNodes) {
   var i = cy.nodes().length;
-  for (let index = i; index < i+numNodes; index++) {
-  addNode(index);
+  for (let index = i; index < i + numNodes; index++) {
+    addNode(index);
   }
   updateChartLayoutGraph();
 }
 
-var addLinks = function(numLinks) {
-  console.log('enteraddlinks');
+var addLinks = function (numLinks) {
   var initLength = cy.nodes().length;
-  for (let index = initLength; index < initLength+numLinks; index++) {
+  for (let index = initLength; index < initLength + numLinks; index++) {
     var newLength = cy.nodes().length;
     var i = getRandomInt(newLength);
     var j = getRandomInt(newLength);
     if (i !== j) {
-      addLink(i,j);
-      console.log(i,j)
+      addLink(i, j);
     }
   }
   updateChartLayoutGraph();
 }
 
-var addOneNodeAndLink = function() {
+var addOneNodeAndLink = function () {
   var i = cy.nodes().length;
-  //for (let index = 0; index < i/10; index++) {
-    var i = cy.nodes().length;
-    var j = getRandomInt(i);
-    addNode(i);
-    addLink(i,j);
-  //}
+  var i = cy.nodes().length;
+  var j = getRandomInt(i);
+  addNode(i);
+  addLink(i, j);
   createDegreeArray();
   cy.layout(options);
 }
 
-
-
-var addNodeAndLink = function() {
+var addNodeAndLink = function () {
   var elem_id = document.getElementById('node_id').value;
   var from_node = document.getElementById('from_node').value;
 
@@ -293,10 +291,10 @@ var addNodeAndLink = function() {
     alert("please fill both input fields");
     return;
   }
-  
+
   addNode(elem_id);
-  addLink(from_node,elem_id)
-  createDegreeArray();  
+  addLink(from_node, elem_id)
+  createDegreeArray();
   cy.layout(options);
 }
 
@@ -319,25 +317,37 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-var initGraphData = function(startNodes) {
-  for ( var i = 0; i <= startNodes; i++ ) {  
-  addNode(i);
+cy.on("tap", "node", (evt) => {
+  //cy.edges().style({ 'line-color': '#1100cc' });
+  evt.cyTarget.connectedEdges().animate({
+    style: {
+      lineColor: "red"
+    }
+  })
+});
+
+
+
+
+var initGraphData = function (startNodes) {
+  for (var i = 0; i <= startNodes; i++) {
+    addNode(i);
   }
-  
+
   function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
 
-  for ( var z = 0; z <= 400; z++ ) { 
-    var i = getRandomInt(startNodes+1);
-    var j = getRandomInt(startNodes+1);
+  for (var z = 0; z <= 400; z++) {
+    var i = getRandomInt(startNodes + 1);
+    var j = getRandomInt(startNodes + 1);
     if (i < j) {
       if (j !== i) {
-        addLink(j,i);
-      } 
+        addLink(j, i);
+      }
     }
   }
-  createDegreeArray();  
+  createDegreeArray();
   cy.layout(options);
 }
 
--- scrollama-cytoscape/steptextcode.js
+++ scrollama-cytoscape/steptextcode.js
@@ -1,13 +1,10 @@
 let steptextcode = [
     {
-        text: "A century ago novelist E M Forster",
-    },
-    {
-        text: "A few preliminaries",
+        text: "A tale of nodes and edges",
     },
     {
         text: "This is a node",
-        code: function(){
+        code: function () {
             addNextNodes(1);
         }
     },
@@ -16,34 +13,111 @@ let steptextcode = [
     },
     {
         text: "This is another node",
-        code: function(){
+        code: function () {
             addNextNodes(1);
         }
     },
     {
         text: "And this is an edge",
-        code: function(){
-            addLink(0,1);
+        code: function () {
+            addLink(0, 1);
         }
     },
     {
         text: "In real life, edges are used to represent relationships between people or things. For example an edge between two people in a social network might represent that the two people have liked or followed one another",
-        code: function(){
-            addLink(0,1);
+        code: function () {
+            addLink(0, 1);
         }
     },
     {
+        comment:"This code often generates graphs with multiple components and or isolated nodes. Intuitively most people probably think of a graph as a single component. Also isolated nodes mess with the sizing / layout of the graph. Recommend change to algo for adding nodes and links accordingly",
         text: "By adding more and more nodes and edges to this graph we can generate a larger and more complex graph",
-        code: function(){
+        code: function () {
             addNextNodes(5);
             addLinks(10);
         }
     },
     {
         text: "But by so doing it also becomes more difficult to intuit the structure or patterns of connectedness of the graph from a simple visualisation",
-        code: function(){
+        code: function () {
             addNextNodes(5);
             addLinks(10);
         }
+    },
+    {
+        text: "Is there some simple property of the graph or its nodes or edges which we could use to gain some additional insight into the graph’s structure?",
+    },
+    {
+        text: "One property mathematicians have studied extensively is the degree of a node in a graph. This is simply a count of the number of edges that each node has",
+    },
+    {
+        comment: "will interactivity confuse the user? before i was scrolling now i'm clicking. do i need to start scrolling again to resume?",
+        comment: "change code which colours edges on clicking a node so that edges previously coloured on selecting previous nodes are returned to the default colour for the graph",
+        text: "Click on a node in the graph at right to highlight it's edges",
+    },
+    {
+        text: "The degree of a node seems intuitively useful as a measure of the relative ‘importance’ of particular nodes in a graph when we consider that nodes with very high degrees or edge counts in real life social networks might be prominent and powerful individuals",
+    },
+    {
+        text: "Next we could calculate the degree for each node in the chart and create a chart which shows how many nodes have zero edges, one edge and so on",
+        code: function () {
+            //insert boilerplate code to eg zoom in on the axes of a chart and back out 
+            // insert code to communicate that bars in the node degree distribution chart are derived from the graph. Eg mouseover bar for degree = 2 in chart, then highlight the nodes in the graph which have this number of edges and then display text saying "3 nodes have 2 edges or degree 2"
+        }
+    },
+    {
+        text: "This chart gives us a view of the graph which is complementary to the simple visualisation of the nodes and edges. Further if we consider links to be the ‘currency’ of a graph, it gives us an insight into how equitably such edges are shared out between the different nodes",
+    },
+    {
+        text: "Random, yes; Senseless, no",
+    },
+    {
+        text: "One challenge faced by mathematicians early to the field of graph theory was how to generate graphs to study",
+    },
+    {
+        text: "One strategy frequently employed by mathematicians is to start by analysing a simple version of a more general problem, because such simple cases are frequently simpler to analyse.",
+    },
+    {
+        text: "In 1959, Paul Erdős and Alfréd Rényi proposed just such a simple method for generating graphs called the random graph model. The rule they proposed is to start with a graph with fixed number of nodes and no edges and then take each of the pairs of nodes in the graph and with a certain probability add an edge between these two edges",
+        code: function () {
+            //insert boilerplate code to eg zoom in on the axes of a chart and back out 
+            // insert code to communicate that bars in the node degree distribution chart are derived from the graph. Eg mouseover bar for degree = 2 in chart, then highlight the nodes in the graph which have this number of edges and then display text saying "3 nodes have 2 edges or degree 2"
+        }
+    },
+    {
+        text: "For example in the following graph we take the first two nodes and ‘toss a coin’ to see if we need to add an edge",
+        code: function () {
+            //Start with graph with eg 5 isolated edges. Then highlight first two edges. Then demonstrate tossing of coin to see if coin added. Heads add. Tails don't add.   
+        }
+    },
+    {
+        text: "We then do the same for each of the other pairs of edges",
+        code: function () {
+            //Repeat for each other pair of edges   
+        }
+    },
+    {
+        text: "It seems intuitive that because the edges are being added to the nodes randomly that each node should end up with about the same number of edges",
+    },
+    {
+        text: "In fact by using the same process to generate larger and larger graphs",
+        code: function () {
+            //Sequentially add more and more nodes and edges to erdos renyi graph OR create sequence of more and more complex random graphs. 
+        }
+    },
+    {
+        text: "We can see that the distribution of edges across nodes becomes closer and closer to a bell curve or normal distribution",
+    },
+    {
+        text: "The normal distribution is, for example, a reasonably accurate approximation for the distribution of heights in a group of people where the bulk of heights are clustered around the average height. But is it a reasonable approximation to the sort of distribution of edges across nodes that we would expect to see, for example, in a real life social network? ",
+        code: function () {
+            //Show Facebook graph with hubs or famous youtuber? 
+        }
+    },
+    {
+        text: "It’s hard to tell for certain just by looking at the graph but it seems that the social network diagram instead has a small number of nodes with very high degree or number of edges - known as hubs - while there are many more nodes which each have very few edges",
+    },
+    {
+        text: "So it seems adding edges to nodes at random doesn’t seem to generate the sort of graphs or social networks that we see in the real world",
     },
 ];
